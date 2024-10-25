8dd83cf7250eb01798a39350096a2d05d0e943c7
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-10-25 19:11:09 +0800

    :sparkles: 安装 `naive-ui` 并配置自动导入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+
 export default defineNuxtConfig({
 	ssr: false,
 	srcDir: 'src',
 	modules: [
-		'@unocss/nuxt'
-	]
+		'@unocss/nuxt',
+		'nuxtjs-naive-ui'
+	],
+	vite: {
+		plugins: [
+			AutoImport({
+				imports: [
+					{
+						'naive-ui': [
+							'useDialog',
+							'useMessage',
+							'useModal',
+							'useNotification',
+							'useLoadingBar'
+						]
+					}
+				]
+			}),
+			Components({
+				resolvers: [
+					NaiveUiResolver()
+				]
+			})
+		]
+	}
 })
