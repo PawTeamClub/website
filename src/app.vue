93ec90bfa5067847d7c2b896e594574c5ce21bac
渣渣120 <WOSHIZHAZHA120@qq.com>
2024-10-25 19:17:08 +0800

    :sparkles: 完成基本布局

---
+++ src/app.vue
@@ -0,0 +1,59 @@
+<script lang="ts" setup>
+import { darkTheme, dateZhCN, lightTheme, useOsTheme, zhCN } from 'naive-ui'
+import avatar from '~/assets/avatar_main.webp'
+
+const osTheme = useOsTheme()
+const theme = computed(() => {
+	switch (osTheme.value) {
+		case 'light':
+			return lightTheme
+		case 'dark':
+			return darkTheme
+		default:
+			return null
+	}
+})
+
+useHead({
+	link: [
+		{
+			rel: 'icon',
+			href: avatar
+		}
+	]
+})
+</script>
+
+<template>
+	<NuxtLayout>
+		<NuxtLoadingIndicator/>
+
+		<n-config-provider :date-locale="dateZhCN" :locale="zhCN" :theme="theme">
+			<n-dialog-provider>
+				<n-loading-bar-provider>
+					<n-message-provider>
+						<n-modal-provider>
+							<n-notification-provider>
+								<n-layout :native-scrollbar="false" position="absolute">
+									<n-layout-content class="container mx-auto">
+										<NuxtPage class="<sm:(px-2) py-10 mb-26"/>
+									</n-layout-content>
+
+									<n-layout-footer class="p-2" position="absolute">
+										<div class="text-center">
+											<n-text>&copy; 2024 - {{ new Date().getFullYear() }}</n-text>
+										</div>
+									</n-layout-footer>
+								</n-layout>
+							</n-notification-provider>
+						</n-modal-provider>
+					</n-message-provider>
+				</n-loading-bar-provider>
+			</n-dialog-provider>
+		</n-config-provider>
+	</NuxtLayout>
+</template>
+
+<style lang="scss">
+@import '~/styles/transitions/page';
+</style>
